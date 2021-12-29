92c20a36fff49556553ac9dfa88414f0f2494339
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2021-12-29 11:18:21 +0200

    My commit message

--- javascript/Page1.js
+++ javascript/Page1.js
@@ -1,29 +1,3 @@
-let apiCardApi = new TempApi.CardApi();import TempApi from '../src/index';window.onload = () => {apiCardApi.getAllcard((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements = document.getElementById("io7yf").querySelectorAll( "[dataitem='true']" );
-  data.forEach((item,i) => {
-    if(subDataElements.length > i)
-      {
-        try { 
-        const htmlElement = subDataElements[i].querySelector("[annotationname = 'cardImage']");
-        if(htmlElement !== null){
-          htmlElement.src = data[data.length -i -1].cardImage;
-        }
-       } catch (e) { console.log(e) };try { 
-        const htmlElement = subDataElements[i].querySelector("[annotationname = 'cardText']");
-        if(htmlElement !== null){
-          htmlElement.textContent = data[data.length -i -1].cardText;
-        }
-       } catch (e) { console.log(e) };try { 
-        const htmlElement = subDataElements[i].querySelector("[annotationname = 'cardDescription']");
-        if(htmlElement !== null){
-          htmlElement.textContent = data[data.length -i -1].cardDescription;
-        }
-       } catch (e) { console.log(e) };try { 
-        const htmlElement = subDataElements[i].querySelector("[annotationname = 'cardButton']");
-        if(htmlElement !== null){
-          htmlElement.undefined = data[data.length -i -1].cardButton;
-        }
-       } catch (e) { console.log(e) };
-        
-      }
-    });
-    [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].remove()})}});};
+document.getElementById('if2ow').onclick = (event) => {
+    event.preventDefault();
+    { location.href= '/Page2';}};
